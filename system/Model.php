7ae25e4fd14dc743d9135f9c729382b7756c31f6
erik-g-z <farmingsimulatorpc@gmail.com>
2020-10-10 21:47:28 -0300

    all

--- system/Model.php
+++ system/Model.php
@@ -6,7 +6,7 @@
             global $config;
 
             try{
-                $this->connect = new PDO("mysql:dbname=".$confgi['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+                $this->connect = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
             }catch(PDOException $e){
                 echo "Erro: ".$e->getMessage();
             }
